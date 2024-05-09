fec5e8b811dcefbfadd084b89ae73a51439f84f3
Daniel <danieltvnguyen@gmail.com>
2024-05-08 23:14:56 -0400

    midi reading

---
+++ midi_file_generation/midi_parser.jl
@@ -0,0 +1,10 @@
+struct MIDIParser
+    # Define your class properties here
+end
+
+# Define methods for the MIDIParser class here
+
+# Example method
+function parse_midi_file(filename::String)
+    # Implement MIDI file parsing logic here
+end
